4f2cf04d7803f0ba59e5ba9689b99f78c70a26d3
muhammadali-ez <muhammadali.ezaccounts.ca>
2026-08-28 14:09:47 -0400

    fix: main ts file uploading limit

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -98,6 +98,10 @@ async function bootstrap() {
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
   await app.listen(process.env.PORT ?? 3010);
+
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 }
 
 bootstrap();
